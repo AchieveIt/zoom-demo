ce0548f08c9d73d85f85ecfc910b42aff641d114
Jameson Hill <jhill@achieveit.com>
2021-02-18 20:44:00 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import faker from 'faker';
+import React from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
+import GridLayout from './Grid/grid';
 
-function App() {
+/*const widgetTypes = ['bar', 'line', 'PITW', 'gauge'];
+
+const Widget = (props) => {
+  const widgetType = useMemo(() => faker.random.arrayElement(widgetTypes), []);
+  return (
+    <div className="widget">
+      {widgetType} ({props.i})
+    </div>
+  );
+};
+
+const Grid = () => {
+  return (
+    <div className="grid">
+      {Array.from({ length: 120 }, (_, i) => (
+        <Widget key={i} i={i + 1} />
+      ))}
+    </div>
+  );
+};*/
+
+const width = 1440;
+export default function App() {
+  const [zoom, setZoom] = useState(1);
+  const [height, setHeight] = useState(0);
+
+  const ref = useRef(null);
+
+  const handler = (e) => {
+    if (e.ctrlKey) {
+      e.preventDefault();
+
+      setZoom((x) =>
+        Math.max(x - parseFloat((e.deltaY * 0.01).toFixed(2)), 0.25)
+      );
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('wheel', handler, {
+      passive: false,
+    });
+    return () =>
+      window.removeEventListener('wheel', handler, {
+        passive: false,
+      });
+  }, []);
+
+  useEffect(() => {
+    if (ref.current) {
+      setHeight(ref.current.getBoundingClientRect().height);
+    }
+  }, []);
+
+  const zoomIn = () => setZoom((x) => parseFloat((x + 0.1).toFixed(2)));
+  const zoomOut = () =>
+    setZoom((x) => Math.max(parseFloat((x - 0.1).toFixed(2)), 0.25));
+
+  const handleChange = (e) => {
+    let val = e.target.value;
+    val /= 100;
+    setZoom(Math.max(parseFloat(val.toFixed(2)), 0));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="header">
+        <div>Zoom</div>
+        <div className="zoomToggle">
+          <button onClick={zoomOut}>-</button>
+          <input
+            value={parseFloat((zoom * 100).toFixed(2))}
+            onChange={handleChange}
+          />
+          <button onClick={zoomIn}>+</button>
+        </div>
+      </div>
+      <div
+        className="container"
+        style={{ margin: 'auto', width: width * zoom, height: height * zoom }}
+      >
+        <div
+          ref={ref}
+          style={{
+            transform: `scale(${zoom}, ${zoom})`,
+            transformOrigin: '0 0 0',
+            width,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <GridLayout zoom={zoom} />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+// Panning: touch-action: pan-x pan-y https://developer.mozilla.org/en-US/docs/Web/CSS/touch-action
---
+++ src/Grid/grid.js
@@ -0,0 +1,74 @@
+import React, { useRef, useState, useReducer } from 'react';
+import GridLayout from 'react-grid-layout';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+import GridElement from './GridElement';
+import layoutReducer from './layoutReducer';
+import initialItems from './items';
+
+const Grid = (props) => {
+  const ref = useRef();
+  const [showAddCursor, setShowAddCursor] = useState(false);
+  const [layout, dispatch] = useReducer(layoutReducer, initialItems);
+
+  document.onclick = (event) => {
+    if (ref.current.contains(event.target)) {
+      if (!event.target.classList.contains('indicator')) {
+        setShowAddCursor(false);
+      }
+    } else {
+      setShowAddCursor(false);
+    }
+  };
+
+  return (
+    <div
+      className={showAddCursor ? 'adding' : undefined}
+      style={{ cursor: showAddCursor ? 'crosshair' : undefined }}
+    >
+      <div ref={ref}>
+        <GridLayout
+          className="layout"
+          layout={layout}
+          onLayoutChange={(layout) => dispatch({ type: 'newLayout', layout })}
+          cols={6}
+          rowHeight={30}
+          width={1440}
+          transformScale={props.zoom}
+        >
+          {layout.map((item) => (
+            <GridElement
+              key={'' + item.i}
+              style={{ border: '1px solid red' }}
+              {...item}
+            >
+              {item.content}
+              <div
+                className="indicator"
+                onClick={() => {
+                  if (showAddCursor) {
+                    setShowAddCursor(false);
+
+                    const newLayout = layout.concat();
+                    newLayout.push({
+                      i: newLayout.length.toString(),
+                      x: item.x,
+                      y: item.y + 1,
+                      w: 1,
+                      h: 1,
+                      content: 'N',
+                    });
+
+                    dispatch({ type: 'newLayout', layout: newLayout });
+                  }
+                }}
+              />
+            </GridElement>
+          ))}
+        </GridLayout>
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
